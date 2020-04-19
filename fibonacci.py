6399870ac76237a21b4d506c021b73d2ca1a794a
AntonyRajeev <63942384+AntonyRajeev@users.noreply.github.com>
2020-04-19 15:09:31 +0530

    fibonnacci.py
    updated code

--- fibonacci.py
+++ fibonacci.py
@@ -4,12 +4,15 @@ j=1
 n=int(input("Enter the number of elements of Fibonacci series to be displayed- "))
 if n<1:
       print("invalid, enter a positive no greater than 0")
-k=0
-while k<n:
+elif n==1:
      print(i)
-     h=i+j
-     i=j
-     j=h
-     k+=1 
+else:      
+     k=0
+     while k<n:
+       print(i)
+       h=i+j
+       i=j
+       j=h
+       k+=1 
      
     
